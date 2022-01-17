4d8563d4938dd62790ca7ebb020cda70afeddaf7
Ophélien Fouques <ophelien.fouques@viacesi.fr>
2022-01-17 16:47:04 +0100

    debut Menu
    Signed-off-by: Ophélien Fouques <ophelien.fouques@viacesi.fr>

--- src/main/Menu.jsx
+++ src/main/Menu.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
+import '../scss/menu.scss';
 
 
 function Menu(props) {
+    const [displayOpt, setDisplayOpt] = useState('none');
+
+    const openMenu = () => {
+        if (displayOpt === 'none')
+            setDisplayOpt('block');
+        else
+            setDisplayOpt('none');
+    };
+
+    const scrollMenu = (menu) => {
+
+    };
 
     return (
         <>
             <header>
-                <h1>CARRLES</h1>
-                <img src={'/icons/logo.png'} alt={'logo'} height={100}/>
+                <h1 className={'titleApp'}>Carles</h1>
+                <img src={'/icons/logo.png'} alt={'logo'} height={100} className={'logo'}/>
             </header>
+            <nav>
+                <div className={'search'}>
+                    <input type={'text'} placeholder={'Rechercher'} className={'inputSearch'}/>
+                    <button onClick={openMenu} className={'settingsBtn'}><img src="/icons/settings.svg" alt="réglage"/>
+                    </button>
+                </div>
+
+                <section className={'optionsMenu'} style={{display: displayOpt}}>
+                    <ul>
+                        <li onClick={scrollMenu('chambre')}>Chambre</li>
+                        <li>SDB</li>
+                        <li>Cuisine</li>
+                        <li>Salle à manger</li>
+                        <li>Salon</li>
+                    </ul>
+                </section>
+            </nav>
 
         </>
     );
